89f2a7710bb2b6310e944abe4db91a8b0b1523f8
moshesimon <moshesimon@outlook.com>
2022-12-07 00:33:13 +0000

    bug fix

--- ArcadeGame/plot.py
+++ ArcadeGame/plot.py
@@ -7,10 +7,8 @@ from config import current_dir
 
 
 PLOT_TITLE = "16 slots env2 end 1 (max=1) vs 16 slots env2 end 2 (max=27)"
+LEGEND = ["DeepEON end lim 1", "DeepEON end lim 3"]
 PLAYER_TYPE = ["agent", "agent"]
-LEGEND = ["DeepEON end 1", "DeepEON end 2"]
-NUMBER_OF_SLOTS_EVALUATED = [16, 16]
-NUMBER_OF_EPISODES_EVALUATED = [1000, 1000]
 NUMBER_OF_SLOTS_TRAINED = [16, 16]
 SCREEN_NUMBER_OF_SLOTS_TRAINED = [16, 16]
 K = [3, 3]
@@ -18,9 +16,11 @@ SOLUTION_REWARD = [10, 10]
 REJECTION_REWARD = [-10, -10]
 GAP_REJECTION_REWARD = [-15, -15]
 SEED = [0, 0]
-END_LIMIT = [1, 27]
-EPISODE_END = [1, 2]
 ENV = [2, 2]
+EPISODE_END = [1, 1]
+END_LIMIT = [1, 3]
+NUMBER_OF_SLOTS_EVALUATED = [16, 16]
+NUMBER_OF_EPISODES_EVALUATED = [1000, 1000]
 AVERAGE_OVER = 50
 
 fig, ax = plt.subplots()
@@ -37,7 +37,7 @@ for i in range(len(PLAYER_TYPE)):
         os.path.join(
             current_dir,
             "Evaluations",
-            f"{PLAYER_TYPE[i]}_evaluation_{NUMBER_OF_SLOTS_TRAINED[i]}_{SCREEN_NUMBER_OF_SLOTS_TRAINED}_{K[i]}_{SOLUTION_REWARD[i]}_{REJECTION_REWARD[i]}_{GAP_REJECTION_REWARD[i]}_{SEED[i]}_{ENV[i]}_{EPISODE_END[i]}_{END_LIMIT[i]}_{NUMBER_OF_SLOTS_EVALUATED[i]}_{NUMBER_OF_EPISODES_EVALUATED[i]}.json",
+            f"{PLAYER_TYPE[i]}_evaluation_{NUMBER_OF_SLOTS_TRAINED[i]}_{SCREEN_NUMBER_OF_SLOTS_TRAINED[i]}_{K[i]}_{SOLUTION_REWARD[i]}_{REJECTION_REWARD[i]}_{GAP_REJECTION_REWARD[i]}_{SEED[i]}_{ENV[i]}_{EPISODE_END[i]}_{END_LIMIT[i]}_{NUMBER_OF_SLOTS_EVALUATED[i]}_{NUMBER_OF_EPISODES_EVALUATED[i]}.json",
         )
     )
     mean_reward = np.mean(df["Episode Rewards"])
